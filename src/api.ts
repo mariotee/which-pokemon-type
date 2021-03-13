61e50ddb1d273beccec9b0ae5c8896619d6771c0
mariotee <mariotorresdev@gmail.com>
2021-03-13 01:15:27 -0600

    added legendary and mythical filters

--- src/api.ts
+++ src/api.ts
@@ -6,7 +6,11 @@ const API_URL = "https://pokeapi.co/api/v2/type";
 export const getPokemonTypeData = async (type: string) => {
     return (await axios.get(`${API_URL}/${type}`)).data;
 }
-
+//trim is temprary until API gets patched
 export const getPokemonData = async (pokemonType: IPokemonFromType) => {
-    return await axios.get(pokemonType.pokemon.url);
+    return axios.get(pokemonType.pokemon.url.replace(/\/$/,''));
+}
+
+export const getSpeciesData = async (pokemonData: any) => {
+    return axios.get(pokemonData.species.url);
 }
